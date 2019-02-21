bd584a771dab05f09aa13e3d5d8aca7455872202
Raymond Schouten <raymondschouten@users.noreply.github.com>
2019-02-21 13:27:03 +0100

    ISSUE-157: Implemented ArrayType

---
+++ symfony-addon-form-types/src/ArrayType.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright (c) 2019 DarkWeb Design
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace DarkWebDesign\SymfonyAddonFormTypes;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Array form field type.
+ *
+ * @author Raymond Schouten
+ *
+ * @since 3.4
+ */
+class ArrayType extends AbstractType
+{
+    /**
+     * Configures the options for this type.
+     *
+     * @param \Symfony\Component\OptionsResolver\OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $compoundNormalizer = function () {
+            return false;
+        };
+
+        $multipleNormalizer = function () {
+            return true;
+        };
+
+        $resolver->setDefaults([
+            'multiple' => true,
+        ]);
+
+        $resolver->setNormalizer('compound', $compoundNormalizer);
+        $resolver->setNormalizer('multiple', $multipleNormalizer);
+    }
+}
